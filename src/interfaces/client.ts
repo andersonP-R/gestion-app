4d6cd3bab87a24a406675bd4fa2da1e8d844857a
Anderson Rivera <anderzonrivera480@gmail.com>
2023-02-20 23:52:01 -0500

    implementations and fixes

---
+++ src/interfaces/client.ts
@@ -0,0 +1,11 @@
+export interface IClient {
+  fullName: string;
+  ID_number: number;
+  address: string;
+  phone: string;
+  neighborhood: string;
+  TotalCredits: number;
+  status: string;
+  route: string;
+  isActive: boolean;
+}
