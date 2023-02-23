0bba3770c811ee79f621b9d92305f2e8365e8eb6
Anderson Rivera <anderzonrivera480@gmail.com>
2023-02-23 15:16:31 -0500

    layout updates

--- src/pages/private-pages/admin/AdminPage.tsx
+++ src/pages/private-pages/admin/AdminPage.tsx
@@ -1,10 +1,13 @@
 import { Grid } from "@mui/material";
 import { FaBiking, FaCoins, FaUserCog } from "react-icons/fa";
+import { DashboardLayout } from "../../../components/layouts";
 import { CardComponent } from "../../../components/ui/admin";
 
 export const AdminPage = () => {
+  // validar el valor del user guadado en las cookies, con el back para permitir el acceso a esta pantalla
+
   return (
-    <>
+    <DashboardLayout title={"Ajustes generales"}>
       <Grid container spacing={4}>
         <CardComponent description={"Usuarios"} url={"/dashboard/admin/users"}>
           <FaUserCog />
@@ -18,6 +21,6 @@ export const AdminPage = () => {
           <FaBiking />
         </CardComponent>
       </Grid>
-    </>
+    </DashboardLayout>
   );
 };
