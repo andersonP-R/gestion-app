1664c18d995a7391c1cc5817a7a0816026e8c3af
Anderson Rivera <anderzonrivera480@gmail.com>
2023-03-08 22:41:46 -0500

    bach script added

---
+++ run-stack.bat
@@ -0,0 +1,2 @@
+start cmd.exe /k "cd C:\Users\WindowsMS\Desktop\ANDERSON\Udemy\Node.js\gestion-app && npm run dev"
+start cmd.exe /k "cd C:\Users\WindowsMS\Desktop\ANDERSON\Udemy\React.js\gestion-app && npm run dev"
